69e05fb69da4f25ef6e7e131b9d18f7fadcadff0
Capella87 <capella87@outlook.com>
2024-11-26 17:17:50 +0900

    chore: Add another normalization unit test method for individual filename input

--- tests/FixNormalization.Tests/FormDNormalizationTests.cs
+++ tests/FixNormalization.Tests/FormDNormalizationTests.cs
@@ -21,7 +21,7 @@ public class FormDNormalizationTests : IClassFixture<FileSystemFixture>
     [InlineData(".\\FirstSubDir\\옛한글")]
     [InlineData(".\\FirstSubDir")]
     [Theory]
-    public async Task FormDFileSystem_Should_Be_Normalized_To_FormC_NonRecursive(string directoryPath)
+    public async Task FormDFileSystem_Directory_Should_Be_Normalized_To_FormC_NonRecursive(string directoryPath)
     {
         // Arrange
         FileSystemFixtureHelpers.ConfigureMockData(fixture, NormalizationForm.FormD);
@@ -29,12 +29,12 @@ public class FormDNormalizationTests : IClassFixture<FileSystemFixture>
         var targetParser = new CommandLineParser<FixCommand>();
 
         // Act
-        var c = targetParser.ParseWithErrorHandling([directoryPath]);
+        var c = targetParser.ParseWithErrorHandling([directoryPath, "-q"]);
 
         var r = await c!.RunAsync(fixture.FileSystem);
 
         // Assertion
-        foreach (var f in this.fixture.FileSystem.Directory.GetFiles(directoryPath))
+        foreach (var f in fixture.FileSystem!.Directory.GetFiles(directoryPath))
         {
             var filename = fixture.FileSystem.Path.GetFileName(f);
 
@@ -46,5 +46,25 @@ public class FormDNormalizationTests : IClassFixture<FileSystemFixture>
             (f) => fixture.FileSystem.Directory.GetFiles(f, "", SearchOption.AllDirectories)
             .All(filePath => !(Path.GetFileName(filePath)
             .IsNormalized(NormalizationForm.FormC))));
+    }
+
+    [InlineData(".\\어린양.txt")]
+    [InlineData(".\\Western\\Áçčèñţşůşîñģdïäçřïţïçš.txt")]
+    [Theory]
+    public async Task FormDFileSystem_File_Should_Be_Normalized_To_FormC(string filePath)
+    {
+        // Arrange
+        FileSystemFixtureHelpers.ConfigureMockData(fixture, NormalizationForm.FormD);
+
+        var targetParser = new CommandLineParser<FixCommand>();
+
+        // Act
+        var c = targetParser.ParseWithErrorHandling([filePath, "-q"]);
+
+        var r = await c!.RunAsync(fixture.FileSystem);
+
+        // Assertion
+        var filename = fixture.FileSystem!.Path.GetFileName(filePath);
+        filename.IsNormalized(NormalizationForm.FormC).Should().BeTrue();
     }
 }
